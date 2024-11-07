c5097741116f68e6641761617d3f558d1b6e02e9
Kyoungjun Park <working@live.co.kr>
2024-11-07 23:49:55 +0900

    .

---
+++ Add-TeamsAppxPackage.ps1
@@ -0,0 +1,5 @@
+﻿
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?linkid=2196106' -OutFile C:\Temp\MSTeams-x64.msix
+Add-AppxPackage -Path C:\Temp\MSTeams-x64.msix
+
+#Get-AppxPackage | where { $_.Name -like 'MSTeams'} | Remove-AppxPackage
