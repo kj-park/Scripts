47a8737256c218c705c09ef108d0d8292e9fe5d4
Kyoungjun Park <working@live.co.kr>
2024-11-07 11:13:03 +0900

    .

---
+++ Disable-sharedPC.ps1
@@ -0,0 +1,6 @@
+﻿$namespaceName = "root\cimv2\mdm\dmmap"
+$parentID="./Vendor/MSFT/Policy/Config"
+$className = "MDM_SharedPC"
+$cimObject = Get-CimInstance -Namespace $namespaceName -ClassName $className
+$cimObject.EnableSharedPCMode = $False
+Set-CimInstance -CimInstance $cimObject
---
+++ Enable-sharedPC.ps1
@@ -0,0 +1,8 @@
+﻿$namespaceName = "root\cimv2\mdm\dmmap"
+$parentID="./Vendor/MSFT/Policy/Config"
+$className = "MDM_SharedPC"
+$cimObject = Get-CimInstance -Namespace $namespaceName -ClassName $className
+$cimObject.EnableSharedPCMode = $True
+$cimObject.AccountModel = 1
+$cimObject.RestrictLocalStorage = $False
+Set-CimInstance -CimInstance $cimObject
