b847f94b5d4048c50f892d574afdfac1a9fc39fe
Kyoungjun Park <working@live.co.kr>
2024-11-14 17:08:05 +0900

    .version 1.21

--- Enable-IntuneEnroll.ps1
+++ Enable-IntuneEnroll.ps1
@@ -679,7 +679,14 @@ function Invoke-AutoEnrollMDM {
     for ( $i = 1; $i -le $MaxCount; $i++ ) {
         $AutoEnrollMDM = Start-Process -FilePath C:\Temp\Intune\PSTools\PsExec64.exe -ArgumentList "-accepteula -nobanner -s C:\Windows\System32\DeviceEnroller.exe /c /AutoEnrollMDM" -PassThru
         $Result = $AutoEnrollMDM.ExitCode
-        if ( $Result -eq 0 ) { return } else { Start-Sleep -Seconds 60 }
+        if ( $Result -eq 0 ) {
+            Write-Host -Object "`t> 'DeviceEnroller.exe /c /AutoEnrollMDM' retruned: $($Result.ToString('x8'))" -ForegroundColor Cyan
+            return
+        } 
+        else {
+            Write-Host -Object "`t> 'DeviceEnroller.exe /c /AutoEnrollMDM' retruned: $($Result.ToString('x8'))" -ForegroundColor Magenta
+            Start-Sleep -Seconds 60
+        }
     }
 }
 
